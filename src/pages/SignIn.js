a9eb5ba78028cf2fc118f5bad74f9eaf9fde7032
Daanish-Khan <dkhan045@uottawa.ca>
2023-02-11 15:32:58 -0500

    Added authentication

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import { useEffect } from 'react';
 import { GoogleButton } from 'react-google-button';
+import { FirebaseContext } from '../context/FirebaseContext';
+import { useNavigate } from 'react-router-dom';
 
 function SignIn() {
+
+    const { googleSignIn, user } = FirebaseContext();
+    const navigate = useNavigate();
+
+    const handleGoogleSignIn = async() => {
+        try {
+            await googleSignIn();
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    // Redirects user to main graph after sign in
+    useEffect(() => {
+        if (user != null) {
+            if (Object.keys(user).length !== 0) {
+                navigate('/graph')
+            }
+        }
+    }, [user]);
+
     return (
         <div>
             <div className='max-w-[240px] m-auto py-4'>
-                <GoogleButton />
+                <GoogleButton onClick={handleGoogleSignIn}/>
             </div>
         </div>
     );
