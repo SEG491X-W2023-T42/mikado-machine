e65aaa8390d0bbaa1187b17e6ad3580b63419f36
Daanish-Khan <dkhan045@uottawa.ca>
2023-02-11 00:57:56 -0500

    Edges + position now grabbed from db

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import { doc, getDoc, getFirestore } from "firebase/firestore";
 
 import 'reactflow/dist/style.css';
 
+// Public credentials
 const firebaseConfig = {
   apiKey: "AIzaSyAqv6_n045I2vtjTIAfDUB9m2l_pru4a6k",
   authDomain: "mikado-method.firebaseapp.com",
@@ -26,31 +27,51 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-/*{ id: '1', position: { x: 0, y: 0}, data: { label: '1'} },
-{ id: '2', position: { x: 0, y: 100 }, data: { label: '2' } }*/
-
-const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
+// Intitial values for the nodes & edges -- DO NOT MAKE THEM EMPTY --
 const initialNodes = [{ id: '1', position: { x: 0, y: 0}, data: { label: '1'} }];
+const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
 
 function App() {
 
   useEffect(() => {
     const getData = async () => {
       
+      // Grab the temp user data
       const docRef = doc(db, "user-1", "graph-1");
       const docSnap = await getDoc(docRef);
   
       if (docSnap.exists()) {
 
+        // Load nodes from db
         const nodeLoad = docSnap.data().node_names;
+        const positionLoad = docSnap.data().positions;
         initialNodes.length = 0;
+        
+        // Construct JSON object
         for (let key in nodeLoad) {
           const node = {};
           node.id = key.toString();
-          node.position = {x: 0, y: 0};
+          node.position = {x: positionLoad[key]['x'], y: positionLoad[key]['y']};
           node.data = { label: nodeLoad[key] };
           initialNodes.push(node);
-          setNodes(initialNodes);
+        }
+        setNodes(initialNodes);
+
+
+        // Load edges from db
+        const edgeLoad = docSnap.data().connections;
+        initialEdges.length = 0;
+
+        // Construct JSON for edges, each has a unique ID 
+        for (let key in edgeLoad) {
+          edgeLoad[key].forEach((item, index) => {
+            const edge = {};
+            edge.id = "e" + key.toString() + "-" + item.toString();
+            edge.source = key.toString();
+            edge.target = item.toString();
+            initialEdges.push(edge);
+            setEdges(initialEdges)
+          });
         }
 
       } else {
@@ -61,6 +82,7 @@ function App() {
     getData();
   }, []);
 
+  // Flow setup
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
 
