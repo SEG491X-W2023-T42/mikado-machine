f62b1419be382244d4f3c0319c0981d6c63cf653
Philippe Gagné <philg2000@gmail.com>
2023-02-15 15:30:49 -0500

    prettier sign-in page (not much prettier, certainly not final)

--- mikado-app/src/pages/SignIn/SignIn.js
+++ mikado-app/src/pages/SignIn/SignIn.js
@@ -1,8 +1,11 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect } from 'react';
 import { GoogleButton } from 'react-google-button';
+// import { GoogleLogin, useGoogleLogin } from '@react-oauth/google';
 import { FirebaseContext } from '../../context/FirebaseContext';
 import { useNavigate } from 'react-router-dom';
+import './SignIn.css';
+import {Helmet} from "react-helmet";
 
 function SignIn() {
 
@@ -27,11 +30,23 @@ function SignIn() {
     }, [user]);
 
     return (
-        <div>
-            <div className='max-w-[240px] m-auto py-4'>
-                <GoogleButton onClick={handleGoogleSignIn}/>
+            <div>
+                <Helmet 
+                bodyAttributes={{style: 'background-color : #0a021c; background-image : radial-gradient(#EEE 1px, transparent 0); background-size: 40px 40px;'}}>
+                </Helmet>
+                <div className='sign-in-card max-w-[240px] m-auto py-4'>
+                    <h1>The Mikado Machine</h1>
+
+                    <button className='google-sign-in-button' onClick={handleGoogleSignIn}>
+                        <img src='https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg' alt='Google Logo' className='google-logo'/>
+                        <p className='google-button-text'>Sign in with Google</p>
+                    </button>
+                    <p className='footnote'>More sign-in options coming soon!</p>
+                    <p className='flavour-text'>The Mikado Machine is an open-source project developed by students at the <a href="https://www.uottawa.ca/en">University of Ottawa</a></p>
+                    <p className='flavour-text'>Learn about the <a href="https://mikadomethod.info/">Mikado Method</a></p>
+                    <p className='github-cta'>Follow our work on <a href="https://github.com/SEG491X-W2023-T42/mikado-machine">GitHub</a></p>
+                </div>
             </div>
-        </div>
     );
 }
 
