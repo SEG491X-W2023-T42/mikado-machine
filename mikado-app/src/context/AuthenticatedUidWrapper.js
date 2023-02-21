e1df1282d46ddfcec6279ca3948ea69de91c086c
Daniel Tang <danielzgtg.opensource@gmail.com>
2023-02-20 22:10:40 -0500

    Simplify the auth guard

--- mikado-app/src/context/AuthenticatedUidWrapper.js
+++ mikado-app/src/context/AuthenticatedUidWrapper.js
@@ -1,28 +1,13 @@
 import { useFirebase } from "./FirebaseContext";
-import { createElement, useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { createElement } from "react";
+import { Navigate } from "react-router-dom";
 
 function AuthenticatedUidWrapper({ wrapped, ...props }) {
   const { user } = useFirebase();
-  const navigate = useNavigate();
-  const [uid, setUid] = useState("");
+  const uid = user?.uid;
 
   // Make sure the user is signed in
-  useEffect(() => {
-    if (user != null) {
-      if (Object.keys(user).length === 0) {
-        navigate('/');
-      } else {
-        setUid(user.uid);
-      }
-    }
-
-    if (user == null) {
-      navigate('/');
-    }
-  }, [navigate, user]);
-
-  return uid && createElement(wrapped, { ...props, uid });
+  return uid ? createElement(wrapped, { ...props, uid }) : <Navigate to={"/"} />;
 }
 
 export default AuthenticatedUidWrapper;
