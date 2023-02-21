938cbe3eeae5bec84a1953a71414a4542c8f9187
Daniel Tang <danielzgtg.opensource@gmail.com>
2023-02-21 07:58:14 -0500

    Fix and simplify hover options

--- mikado-app/src/pages/Graph/MyNode.js
+++ mikado-app/src/pages/Graph/MyNode.js
@@ -1,18 +1,15 @@
-import { ConnectionMode, Handle, NodeToolbar, Position } from "reactflow";
-import { useState } from "react";
+import { ConnectionMode, Handle, NodeToolbar } from "reactflow";
 
 export const MY_NODE_CONNECTION_MODE = ConnectionMode.Loose;
 
 function MyNode({ data }) {
-  const [isVisible, setVisible] = useState(false);
-
-  return <div onMouseEnter={() => setVisible(true)} onMouseLeave={() => setVisible(false)}>
-    <NodeToolbar isVisible={isVisible} position={Position.Top}>
+  return <>
+    <NodeToolbar>
       <button>Hello</button>
     </NodeToolbar>
     {data.label}
     <Handle />
-  </div>
+  </>
 }
 
 export default MyNode;
