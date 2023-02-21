d3aee000c6b3f9e8eab43bb612721e415daf0f79
Daniel Tang <danielzgtg.opensource@gmail.com>
2023-02-21 09:38:17 -0500

    Implement add node button

--- mikado-app/src/pages/Graph/MyDrawer.js
+++ mikado-app/src/pages/Graph/MyDrawer.js
@@ -1,6 +1,14 @@
 import { SwipeableDrawer } from "@mui/material";
 import "./MyDrawer.css";
 
+export const DRAG_AND_DROP_MIME = "application/mikado-app";
+export const DRAG_AND_DROP_MAGIC = "mikado-mikado";
+
+function onDragStart(event) {
+  event.dataTransfer.setData(DRAG_AND_DROP_MIME, DRAG_AND_DROP_MAGIC);
+  event.dataTransfer.effectAllowed = "move";
+}
+
 function MyDrawer({ selectionData }) {
   // TODO fix the swiping on desktop
   return <SwipeableDrawer
@@ -15,7 +23,7 @@ function MyDrawer({ selectionData }) {
     {selectionData ?
       "Selected"
       :
-      "Not Selected"
+      <div onDragStart={onDragStart} draggable>Add Node</div>
     }
   </SwipeableDrawer>
 }
