d1b6d914f8dd75a3eadf957248726f831e540c85
Daniel Tang <danielzgtg.opensource@gmail.com>
2023-02-21 05:19:54 -0500

    Inhale the Vertex ID copium

--- mikado-app/src/viewmodel/autoincrement.js
+++ mikado-app/src/viewmodel/autoincrement.js
@@ -3,7 +3,7 @@
  *
  * With at least 53 bits (Number.MAX_SAFE_INTEGER, while the theoretical float is 64 bits but has extra decimal considerations) of
  * precision, this should not run out during the time the tab is open. This value should not be persisted, and it shall be reset
- * when the application is restarted. If it managed to become a problem, then we can always return objects to use as unique instances instead.
+ * when the application is restarted. If it managed to become a problem, then we can always return strings to use as unique instances instead.
  *
  * This was originally created for the depth-first-search. The naive approach is to declare a visited boolean, clear it before the search,
  * and set it during the search. This is not very efficient or cache-friendly as it goes through memory twice. Furthermore, a number and
@@ -14,7 +14,8 @@
 let counter = 0;
 
 /**
- * Generates an autoincremented value. The type must not be relied on, only the fact that values from different calls to this function are different.
+ * Generates an autoincremented value. The type must not be relied on, only the fact that values from different calls to this function are different,
+ * and that .toString() exists and is different iff the value is different.
  */
 export default function generateAutoincremented() {
   return counter++;
--- mikado-app/src/viewmodel/serde.js
+++ mikado-app/src/viewmodel/serde.js
@@ -1,7 +1,7 @@
-import { MarkerType } from "reactflow";
 import { connectFirestoreEmulator, doc, getDoc, getFirestore, setDoc } from "firebase/firestore";
 import { firebase, USING_DEBUG_EMULATORS } from '../firebase';
 import { runtime_assert } from "./assert";
+import generateAutoincremented from "./autoincrement";
 
 const db = getFirestore(firebase);
 if (USING_DEBUG_EMULATORS) {
@@ -38,13 +38,24 @@ export async function loadFromDb(uid) {
   // TODO add a version key and prevent loading newer schemas
   const { node_names, positions, connections } = docSnap.data();
 
+  /**
+   * Lookup table so that newEdges can follow the remapped ids in newNodes.
+   *
+   * This cuts the IDs used in the database free from the IDs passed to React Flow.
+   * Objects in JavaScript are HashMaps (Arrays are only an optimization) so
+   * O(1) average but O(n) worst case access.
+   * The good thing compared to an array of sequential indices is that
+   * one can avoid O(n) renumberings when trying to delete the middle element.
+   * With autoincrement, adding a node is also guaranteed not to collide.
+   */
+  const databaseKeysToNewIdsLookup = {};
   // Load nodes from db
   const newNodes = Object.entries(node_names).map(([key, label]) => {
     const { x, y } = positions[key];
     // Construct object for React Flow
     // Coerce everything to the expected types to ignore potential database schema changes
     return {
-      id: key.toString(),
+      id: (databaseKeysToNewIdsLookup[key] = generateAutoincremented().toString()),
       position: { x: +x, y: +y },
       data: { label: label.toString() },
     };
@@ -52,11 +63,11 @@ export async function loadFromDb(uid) {
 
   // Load edges from db
   const newEdges = Object.entries(connections).flatMap(([key, values]) => {
-    const source = key.toString();
+    const source = databaseKeysToNewIdsLookup[key];
     // Map through outgoing connections
     // Remember that these are directed edges, so {1:[2]} != {2:[1]}
     return values.map(value => {
-      const target = value.toString();
+      const target = databaseKeysToNewIdsLookup[value];
       // Construct JSON for edges, each has a unique ID
       return { id: `e${source}-${target}`, source, target };
     });
@@ -71,24 +82,49 @@ export async function loadFromDb(uid) {
 export async function saveToDb(nodes, edges, uid) {
   // Construct objects for database
 
+  /**
+   * Lookup table to pack nodes back into sequential numbering.
+   *
+   * Again, this completely renumbers everything. This is necessary to remove gaps and
+   * prevent numbers in the database from hitting space limits by getting too large.
+   *
+   * String idempotency is currently present but may not be guaranteed in the future.
+   * That is, a load-save-load guarantees the graphs are isomorphic but there is no
+   * guarantee that the IDs won't be swapped. There is currently no code that purposefully
+   * swaps them and ES2015/ES2020 makes object iteration deterministic, but don't rely on this.
+   *
+   * At the very least this may affect z-ordering. This avoided by prohibiting overlapping nodes.
+   * Dragging is not a problem as it brings nodes to the top. However, this logic may be changed
+   * in the future, in addition to sizes depending on installed browser fonts and this being
+   * unenforceable server-side.
+   *
+   * Another challenge may be collaborative/offline editing. That feature probably won't be
+   * implemented for a while, so last-write-wins on the scale of the entire graph should be
+   * used for now to simplify things. The problem is that it may be harder to diff changed
+   * graphs if the IDs are scrambled or to keep track of what node the current or remote
+   * user has selected such as for editing. But there was always the duplicate new node ID
+   * problem that existed when the IDs were previously sequential, and that won't even be
+   * probabilistically solved until moving to UUIDs per node.
+   */
+  const newIdsToDatabaseKeysLookup = {};
   // Serialize the nodes
   // For some reason, the database is in Struct-Of-Arrays layout even though it's NoSQL
   const node_names = {};
   const positions = {};
-  nodes.forEach((node) => {
-    // By the way, for some reason the database is one-indexed
-    const key = node.id;
+  nodes.forEach((node, index) => {
+    newIdsToDatabaseKeysLookup[node.id] = index;
     // No need to coerce as we own node.data: T for Node<T>
-    node_names[key] = node.data.label;
+    node_names[index] = node.data.label;
     // Assuming there is no reason React Flow will change away from { x: number, y: number }
-    positions[key] = node.position;
+    positions[index] = node.position;
   });
 
   const connections = {};
   // Loop thru every connection and add to a map
   for (const edge of edges) {
-    const { source, target } = edge;
-    (connections[source] = connections[source] || []).push(target);
+    const src = newIdsToDatabaseKeysLookup[edge.source];
+    const dst = newIdsToDatabaseKeysLookup[edge.target];
+    (connections[src] = connections[src] || []).push(dst);
   }
 
   const data = { connections, node_names, positions };
