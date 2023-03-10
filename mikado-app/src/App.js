baa8ba25e9094ea810e46a552bf4bbd325fa13d3
Daanish-Khan <dkhan045@uottawa.ca>
2023-03-09 19:12:45 -0500

    lint test

--- mikado-app/src/App.js
+++ mikado-app/src/App.js
@@ -11,7 +11,7 @@ function App() {
       <Routes>
         <Route path='/' element={<SignIn />} />
         <Route path='/graph' element={<GraphPage />} />
-      </Routes>
+      </Routes> 
     </FirebaseContextProvider>
   );
 }
