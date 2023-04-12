ff318b8eb6c4cabf06eec65f1109f40985ef1bed
Daanish-Khan <dkhan045@uottawa.ca>
2023-04-12 11:44:34 -0400

    add revert back to ready after complete

--- mikado-app/src/pages/Graph/CompleteNode.js
+++ mikado-app/src/pages/Graph/CompleteNode.js
@@ -2,6 +2,7 @@ import { ConnectionMode, Handle, NodeToolbar } from "reactflow";
 import useDisplayLayerStore from "../../viewmodel/displayLayerStore";
 import DeleteIcon from '@mui/icons-material/Delete';
 import CheckIcon from '@mui/icons-material/Check';
+import HighlightOffIcon from '@mui/icons-material/HighlightOff';
 
 export const MY_NODE_CONNECTION_MODE = ConnectionMode.Loose;
 
@@ -16,6 +17,9 @@ function LockedNode({ id, data }) {
       <NodeToolbar>
         <button onClick={() => operations.deleteNode(id)}>
           <DeleteIcon />
+        </button>
+        <button onClick={() => operations.setNodeCompleted(id, false)}>
+            <HighlightOffIcon />
         </button>
       </NodeToolbar>
         {data.label}
