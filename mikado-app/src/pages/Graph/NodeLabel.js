4dfd1828be4da716aa6af96d1b82ac235c51a0f1
Daniel Tang <danielzgtg.opensource@gmail.com>
2023-09-26 19:48:10 -0400

    refactor: Extract SeamlessEditor.js

--- mikado-app/src/pages/Graph/NodeLabel.js
+++ mikado-app/src/pages/Graph/NodeLabel.js
@@ -1,5 +1,5 @@
-import { useEffect, useRef, useState } from "react";
 import { useStoreHack } from "../../StoreHackContext";
+import SeamlessEditor from "../../components/SeamlessEditor";
 
 const selector = (state) => {
   const {editingNodeId, editingNodeInitialValue, editNode, operations} = state;
@@ -8,56 +8,17 @@ const selector = (state) => {
 
 export default function NodeLabel({ id, label }) {
   const {editingNodeId, editingNodeInitialValue, editNode, operations} = useStoreHack()(selector);
-  const [text, setText] = useState(label);
-  const ref = useRef(void 0);
-  const wasComposingRef = useRef(false);
   const editing = editingNodeId === id;
-  useEffect(() => {
-    if (editing) {
-      setText(editingNodeInitialValue);
-      ref.current?.focus();
-      ref.current?.select();
-    }
-  }, [editing]);
-  useEffect(() => {
-    const { current } = ref;
-    if (!current) return;
-    const { style } = current;
-    // Shrink and grow based on content
-    style.height = 0;
-    style.height = current.scrollHeight + "px";
-  }, [text]);
-  if (!editing && text !== label) {
-    setText(label);
-  }
-  function finishEditing() {
-    editNode("", "");
-    const filteredText = text.replaceAll('\n', ' ');
-    operations.setNodeLabel(id, filteredText);
-    setText(filteredText);
-  }
   // <input> is single link
   // <div contenteditable> is an accessibility and security problem
-  return <textarea
-    autoFocus={editing}
-    disabled={!editing}
-    value={text}
-    rows={1}
-    onChange={(e) => {setText(e.target.value)}}
-    onKeyDown={(e) => {
-      // Don't break IMEs
-      wasComposingRef.current = e.nativeEvent.isComposing;
+  return <SeamlessEditor
+    label={label}
+    editing={editing}
+    initialValue={!editing ? "" : editingNodeInitialValue}
+    onFinishEditing={(filteredText) => {
+      editNode("", "");
+      operations.setNodeLabel(id, filteredText);
     }}
-    onKeyUp={(e) => {
-      if (e.key !== "Enter") return;
-      if (wasComposingRef.current) return;
-      finishEditing();
-    }}
-    onBlur={finishEditing}
-    ref={ref}
+    singleLine={false}
   />
-
-  // TODO: performance
-  // - Use HTMLInputElement over TextNode only when necessary
-  // - Find a way so that targeting a node for editing does not force all nodes to rerender. Perhaps optimize the store access a bit
 }
