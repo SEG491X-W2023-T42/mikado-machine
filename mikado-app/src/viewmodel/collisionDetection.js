a0d7051a9d4a5e0c8836fe09fe31c494b3c0de38
Daniel Tang <danielzgtg.opensource@gmail.com>
2023-09-27 17:32:44 -0400

    fix: Don't give up placing nodes nearer

--- mikado-app/src/viewmodel/collisionDetection.js
+++ mikado-app/src/viewmodel/collisionDetection.js
@@ -9,7 +9,7 @@ export default function createIntersectionDetectorFor(nodeLike) {
     // Convert other node's size offsets into absolute coordinates
     const { x: otherLeft, y: otherTop } = other.position;
     const otherRight = otherLeft + other.width, otherBottom = otherTop + other.height;
-
+/*
     if (!(other.id === id // Ignore self-intersection, or else it will always return true
     || myRight < otherLeft // I'm to your right, with a vertical line between us
     || myLeft > otherRight // I'm to your left, with a vertical line between us
@@ -34,12 +34,14 @@ export default function createIntersectionDetectorFor(nodeLike) {
     // console.log(myTop > otherBottom)
     // console.log(myBottom < otherTop)
   }
+  */
     // To know when two nodes intersect, just consider the contrapositive
+    // The intervals are closed towards the top left, and open towards the bottom right
     return !(other.id === id // Ignore self-intersection, or else it will always return true
-      || myRight < otherLeft // I'm to your right, with a vertical line between us
-      || myLeft > otherRight // I'm to your left, with a vertical line between us
-      || myTop > otherBottom // I'm below you, with a horizontal line between us
-      || myBottom < otherTop // I'm above you, with a horizontal line between us
+      || myRight <= otherLeft // I'm to your right, with a vertical line between us
+      || myLeft >= otherRight // I'm to your left, with a vertical line between us
+      || myTop >= otherBottom // I'm below you, with a horizontal line between us
+      || myBottom <= otherTop // I'm above you, with a horizontal line between us
     );
   };
 }
