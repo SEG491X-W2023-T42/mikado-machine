679155237a37a916864ab52728bb0dbaed306bdd
Daniel Tang <danielzgtg.opensource@gmail.com>
2023-09-28 03:05:11 -0400

    refactor: Unify MyNode

--- mikado-app/src/pages/Graph/MyNode.js
+++ mikado-app/src/pages/Graph/MyNode.js
@@ -1,6 +1,8 @@
-import { ConnectionMode } from "reactflow";
-// import { ConnectionMode, Handle, NodeToolbar } from "reactflow";
-// import NodeToolbarCommon from "./NodeToolbarCommon";
+import { ConnectionMode, Handle, NodeToolbar } from "reactflow";
+import { useEnterGraphHack } from "./EnterGraphHackContext";
+import { runtime_assert } from "../../viewmodel/assert";
+import { useStoreHack } from "../../StoreHackContext";
+import SeamlessEditor from "../../components/SeamlessEditor";
 
 export const MY_NODE_CONNECTION_MODE = ConnectionMode.Loose;
 
@@ -18,3 +20,56 @@ export const MY_NODE_CONNECTION_MODE = ConnectionMode.Loose;
 // }
 //
 // export default MyNode;
+const selector = (state) => {
+  const { editingNodeId, editingNodeInitialValue, editNode, operations } = state;
+  return { editingNodeId, editingNodeInitialValue, editNode, operations };
+};
+
+const ARIA_LABELS = {
+  "complete": "Completed",
+  "goal": "Goal",
+  "locked": "Blocked",
+  "ready": "Ready",
+};
+
+export default function MyNode({ id, data, type }) {
+  const { editingNodeId, editingNodeInitialValue, editNode, operations } = useStoreHack()(selector);
+  const editing = editingNodeId === id;
+
+  const subgraph = operations?.isNodeSubgraph(id);
+  const enterGraph = useEnterGraphHack();
+
+  const goal = type === "goal";
+  const locked = type === "locked";
+  const completed = type === "complete";
+  const ariaLabel = ARIA_LABELS[type];
+  runtime_assert(ariaLabel);
+
+  return <>
+    <NodeToolbar>
+      <button aria-label="Delete" data-icon="delete" onClick={() => operations.deleteNode(id)} />
+      {goal ? null : <button
+        aria-label={subgraph ? "Enter subgraph" : "Create subgraph"}
+        data-icon={subgraph ? "enter-subgraph" : "create-subgraph"}
+        onClick={() => enterGraph((uid) => operations.createSubgraphAndSaveIfNotExists(uid, id))}
+      />}
+      {goal || locked ? null : <button
+        aria-label={completed ? "Mark incomplete" : "Mark completed"}
+        data-icon={completed ? "incomplete" : "completed"}
+        onClick={() => void operations.setNodeCompleted(id, !completed)}
+      />}
+    </NodeToolbar>
+    <SeamlessEditor
+      label={data.label}
+      editing={editing}
+      initialValue={!editing ? "" : editingNodeInitialValue}
+      onFinishEditing={(filteredText) => {
+        editNode("", "");
+        operations.setNodeLabel(id, filteredText);
+      }}
+      singleLine={false}
+    />
+    <Handle />
+    <div aria-label={ariaLabel} />
+  </>;
+}
--- mikado-app/src/pages/Graph/graphTheme.js
+++ mikado-app/src/pages/Graph/graphTheme.js
@@ -1,9 +1,6 @@
 import { MarkerType } from "reactflow";
-import GoalNode from "./GoalNode";
-import ReadyNode from "./ReadyNode";
 import FloatingEdge from "./FloatingEdge";
-import LockedNode from "./LockedNode";
-import CompleteNode from "./CompleteNode"
+import MyNode from "./MyNode";
 
 export const CONNECTION_LINE_STYLE = {
   strokeWidth: 3,
@@ -36,8 +33,8 @@ export const NODE_TYPES = {
    * Override default (and, so far, only) node type
    */
   default: AssertFalseNode,
-  goal: GoalNode,
-  ready: ReadyNode,
-  locked: LockedNode,
-  complete: CompleteNode,
+  goal: MyNode,
+  ready: MyNode,
+  locked: MyNode,
+  complete: MyNode,
 }
