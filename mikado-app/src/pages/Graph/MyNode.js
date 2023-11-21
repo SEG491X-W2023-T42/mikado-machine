0ee38cf7aba36d6eefca8c6fdba9f1b19222e7b6
Philippe Gagné <philg2000@gmail.com>
2023-11-21 17:21:22 -0500

    Added subgraph nav buttons to nodes and partial logic

--- mikado-app/src/pages/Graph/MyNode.js
+++ mikado-app/src/pages/Graph/MyNode.js
@@ -4,6 +4,7 @@ import { runtime_assert } from "../../viewmodel/assert";
 import { useStoreHack } from "../../StoreHackContext";
 import SeamlessEditor from "../../components/SeamlessEditor";
 import { getGatekeeperFlags } from "../../viewmodel/gatekeeper";
+import { ArrowDownward, ArrowUpward} from '@mui/icons-material';
 
 export const MY_NODE_CONNECTION_MODE = ConnectionMode.Loose;
 
@@ -27,18 +28,21 @@ export default function MyNode({ id, data, type, exporting = false, ...rest }) {
   const subgraph = operations?.isNodeSubgraph(id);
   const enterGraph = useEnterGraphHack();
 
+  const isInSubgraph = operations?.isNodeInSubgraph();
+
   const notGoal = type !== "goal";
   const notLocked = type !== "locked";
   const completed = type === "complete";
   const ariaLabel = ARIA_LABELS[type];
   runtime_assert(ariaLabel);
 
+
   const result = <>
     {exporting ? null : <NodeToolbar>
       {allowRemoveNode && <button aria-label="Delete" data-icon="delete" onClick={() => operations.deleteNode(id)} />}
-      {allowSubgraph && notGoal && <button
-        aria-label={subgraph ? "Enter subgraph" : "Create subgraph"}
-        data-icon={subgraph ? "enter-subgraph" : "create-subgraph"}
+      {allowSubgraph && notGoal && !subgraph && <button
+        aria-label= "Create subgraph"
+        data-icon="create-subgraph"
         onClick={() => enterGraph((uid) => operations.createSubgraphAndSaveIfNotExists(uid, id))}
       />}
       {allowModifyNodeCompletion && notGoal && notLocked && <button
@@ -47,19 +51,37 @@ export default function MyNode({ id, data, type, exporting = false, ...rest }) {
         onClick={() => void operations.setNodeCompleted(id, !completed)}
       />}
     </NodeToolbar>}
-    <SeamlessEditor
-      label={data.label}
-      editing={editing}
-      exporting={exporting}
-      initialValue={!editing ? "" : editingNodeInitialValue}
-      onFinishEditing={(filteredText) => {
-        editNode("", "");
-        operations.setNodeLabel(id, filteredText);
-      }}
-      singleLine={false}
-    />
+
+    <div className="content-wrapper">
+      <SeamlessEditor
+        label={data.label}
+        editing={editing}
+        exporting={exporting}
+        initialValue={!editing ? "" : editingNodeInitialValue}
+        onFinishEditing={(filteredText) => {
+          editNode("", "");
+          operations.setNodeLabel(id, filteredText);
+        }}
+        singleLine={false}
+      />
+      <div className="node-icon-wrapper">
+        <div aria-label={ariaLabel} />
+      </div>
+    </div>
+    
     {exporting ? null : <Handle />}
-    <div aria-label={ariaLabel} />
+    {(subgraph || (isInSubgraph && !notGoal)) && //if node has a subgraph, or if a node is the root node of a subgraph
+      <button className="enter-exit-subgraph-button" // button to enter or exit subgraph
+        aria-label = {notGoal? "Enter subgraph" : "Return to parent graph"}
+        onClick= {notGoal? () => enterGraph((uid) => operations.createSubgraphAndSaveIfNotExists(uid, id)) 
+                  : console.log("Returning to parent graph") //DISH : here is where return-to-parent logic needs to be.
+                }
+      >
+        {notGoal? <ArrowDownward sx={{fontSize:16}} /> : <ArrowUpward sx={{fontSize:16}}/>}
+        {notGoal? "Enter subgraph" : "Return to parent graph"}
+      </button>
+    }
+
   </>;
   if (exporting) {
     const { position, positionOffset } = rest; // Exists when exporting only
