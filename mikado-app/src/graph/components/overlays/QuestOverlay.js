414b369bd19590b5bdd430cc6abb68d4da30cb55
Daanish-Khan <dkhan045@uottawa.ca>
2023-11-20 22:47:08 -0500

    disable see more button for now since it doesnt do anything

--- mikado-app/src/graph/components/overlays/QuestOverlay.js
+++ mikado-app/src/graph/components/overlays/QuestOverlay.js
@@ -8,7 +8,7 @@ export default function QuestOverlay({ currentTask, completeClick }) {
 			<h3>{currentTask == undefined || currentTask.length == 0 ? "Connect a non-complete node to the Goal Node to start a new Quest!" : currentTask[0].data.label}</h3>
 			<ButtonGroup variant="contained">
 				<Button sx={{fontFamily: 'Inter', fontWeight: 'bold'}} onClick={completeClick} disabled={currentTask == undefined || currentTask.length == 0}>Complete</Button>
-				<Button sx={{fontFamily: 'Inter', fontWeight: 'bold'}}>See More</Button>
+				{/*<Button sx={{fontFamily: 'Inter', fontWeight: 'bold'}} disabled>See More</Button>*/}
 			</ButtonGroup>
 		</div>
 	)
