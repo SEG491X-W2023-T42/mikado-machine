686bfe79a85b3976e5e1d595a3b1af699cc6cfae
Daniel Tang <danielzgtg.opensource@gmail.com>
2023-11-23 15:52:45 -0500

    feat: Add questline modal outline

--- mikado-app/src/graph/components/overlays/QuestModal.js
+++ mikado-app/src/graph/components/overlays/QuestModal.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Dialog, DialogActions, DialogTitle, Button, styled, IconButton, Typography, DialogContent } from '@mui/material';
+import { Dialog, DialogActions, DialogTitle, Button, styled, IconButton, Typography, DialogContent, Grid, List, ListItem } from '@mui/material';
 import { Close } from '@mui/icons-material';
 
 export default function QuestModal({ open, handleClose }) {
@@ -36,13 +36,23 @@ export default function QuestModal({ open, handleClose }) {
 				<Close />
 			</IconButton>
 			<DialogContent dividers>
-				Section 1
+        <h1>Current Quest {/*TODO %*/}</h1>
+        <Grid container justifyContent="space-between">
+          <Grid item>Mytask {/*Current Task*/}</Grid>
+          <Grid item>Mytask {/*Next Task*/}</Grid>
+          <Grid item>{/* Spacer */}</Grid>
+        </Grid>
 			</DialogContent>
 			<DialogContent dividers>
-				Section 2
+        <h1>Other Quests {/*TODO %*/}</h1>
+        <List disablePadding>
+          <ListItem>Mytask {/*TODO %*/}</ListItem>
+          <ListItem>Mytask {/*TODO %*/}</ListItem>
+          <ListItem>Mytask {/*TODO %*/}</ListItem>
+        </List>
 			</DialogContent>
 			<DialogActions>
-				<Button onClick={handleClose} autoFocus>Save changes</Button>
+				<Button onClick={handleClose} autoFocus>Back</Button>
 			</DialogActions>
 		</StyledDialog>
 	);
